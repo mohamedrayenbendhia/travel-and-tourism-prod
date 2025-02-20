3b9ddf322442428ac4f6b8b8b145a54b9f8c4d2d
mohamedrayenbendhia <mohamedrayen.bendhia@esprit.tn>
2025-02-20 21:12:04 +0100

    - Ajout du fichier .env.example et mise à jour de la configuration DATABASE_URL. - Suppression des anciens fichiers de migration et génération de nouveau fichier.

--- migrations/Version20250220182816.php
+++ migrations/Version20250220182816.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250213152550 extends AbstractMigration
+final class Version20250220182816 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,13 @@ final class Version20250213152550 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE logement ADD image VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE logement DROP image');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX UNIQ_IDENTIFIER_EMAIL');
     }
 }
